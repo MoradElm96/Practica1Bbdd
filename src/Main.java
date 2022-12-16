278e0843f49dd1f7f45f33de27ea149534d5b475
alumno <alumno@192.168.194.131>
2022-12-16 09:52:39 +0100

    third

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -26,38 +28,88 @@ public class Main {
             String user = "root";
             String password = "";
             Connection con = DriverManager.getConnection(url, user, password);
-            
-            //usamos la tabla instituto
-            Statement st= con.createStatement();
-            st.execute("use instituto");
-            
-            
-            String crearTabla="CREATE TABLE `instituto`.`notasfinales` ( `MAT` VARCHAR(20) NOT NULL , `cod` INT(10) NOT NULL , `NotaMedia` INT(10) NOT NULL ) ENGINE = InnoDB;";
-            //creamos la tabla
-            PreparedStatement st1 = con.prepareStatement(crearTabla);
-            st1.execute();
-            st1.close();
-            
-            Statement st2 = con.createStatement();
-            st2.execute("use notas");
-            
-            
-            
-            
-            
-            st.close();
-            
 
-           
-            
-            
-            
+            Statement stUsarBase = con.createStatement();
+            Statement st1crearTabla = con.createStatement();
+            Statement stConsulta = con.createStatement();
+            Statement stInsertar = con.createStatement();
+            Statement stSelect = con.createStatement();
+
+            //usamos la tabla instituto
+            stUsarBase.execute("use instituto");
+
+            String cadena = "create table NotasFinales("
+                    + "mat varchar(10),"
+                    + "cod tinyint(2),"
+                    + "notamedia decimal(4,2),"
+                    + "primary key(mat,cod))";
+            //ver esta parte
+
+            System.out.println("tabla no existe, se esta creando...");
+            st1crearTabla.execute(cadena);
+
+            cadena = "select * from notas";
+            ResultSet rs = stConsulta.executeQuery(cadena);
+
+            String mat;
+            Double media;
+            int codigo;
+
+            while (rs.next()) {
+                mat = rs.getString(1);//porque es el campo uno
+                codigo = rs.getInt(2);
+                media = (rs.getInt(3) + rs.getInt(4) + rs.getInt(5)) / 3.0;
+                cadena = "INSERT INTO `notasfinales`(`mat`, `cod`, `notamedia`) VALUES ('" + mat + "'," + codigo + "," + media + ")";
+                stInsertar.executeUpdate(cadena);
+            }
+            cadena = "select APEL_NOM,NOMBRE,NOTA1,NOTA2,NOTA3,notamedia "
+                    + "from notas nt,alumnos al,asignaturas asig,notasfinales ntf "
+                    + "where nt.mat=al.mat and nt.cod=asig.cod and ntf.mat=nt.mat "
+                    + "and ntf.cod=nt.cod";
+            rs = stSelect.executeQuery(cadena);
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2)
+                        + " " + rs.getInt(3) + " " + rs.getInt(4) + " " + rs.getInt(5)
+                        + " " + rs.getDouble(6));
+            }
+
+            st1crearTabla.close();
+            stUsarBase.close();
+            stConsulta.close();
+            stInsertar.close();
+            stSelect.close();
+
         } catch (ClassNotFoundException e) {
             System.out.println(e);
 
         } catch (SQLException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    
+    
+    //metodo para comprobar si tabla existe
+    public static boolean tableExist(Connection con, String tableName) {
+
+        boolean tExists = false;
+
+        try {
+
+            ResultSet rs = con.getMetaData().getTables(null, null, tableName, null);
+            while (rs.next()) {
+                String tName = rs.getString("TABLE_NAME");
+                if (tName != null && tName.equals(tableName)) {
+                    tExists = true;
+                    break;
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return tExists;
     }
 
 }
